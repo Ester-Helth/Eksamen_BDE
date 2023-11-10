4c54957cea5a0ab601becfe4dad374ab21e896dc
Ester-Helth <118178505+Ester-Helth@users.noreply.github.com>
2023-11-10 14:11:00 -0600

    finish

--- test.js
+++ test.js
@@ -3,25 +3,25 @@ import { Selector } from "testcafe"
 fixture`#1 Testing the converter`
     .page('./index.html');
 
-     // testing miles to km input
-    // test("Converter to km", async t => {
-    //     await t
-    //     .typeText(Selector("#milesInput"), '12')
+    // testing miles to km input
+    test("Converter to km", async t => {
+        await t
+        .typeText(Selector("#milesInput"), '12')
 
-    //     // Act
-    //     .click("#toKmButton")
-    // });
+        // Act
+        .click("#toKmButton")
+    });
 
     // #2 testing km to miles input (evt plus converter)
 
-    test("Converter to miles", async t => {
-        await t
-        .typeText(Selector("#kmInput"), '19.308')
+    // test("Converter to miles", async t => {
+    //     await t
+    //     .typeText(Selector("#kmInput"), '19.308')
 
-        // Act
-        .click("#toMilesButton")
+    //     // Act
+    //     .click("#toMilesButton")
 
-        // Check if output number is converted correctly
-        .expect(Selector("#resultInMiles").innerText).contains('12');
-    });
+    //     // Check if output number is converted correctly
+    //     .expect(Selector("#resultInMiles").innerText).contains('12');
+    // });
 
---
+++ tsExamples.ts
@@ -0,0 +1,19 @@
+//Explicit Types
+    // let name: string;
+    // let age: number;
+    // isLoggedIn: boolean;
+
+    // let apples: string[] = [];
+    // apples.push('lilly');
+    // apples.push('jane');
+
+
+// Union types
+    // let mixed (string|number|boolean)[]=[];
+    // mixed.push(28);
+    // console.log(mixed);
+
+    // let pie: string|number;
+    // pie = 12;
+    // pie = '12';
+
